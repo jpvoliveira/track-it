7e21d69ca2140991f71f387742be4164e55faabf
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2022-01-05 01:08:05 -0300

    falta armazenar o token

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Register from "./components/Register/Index";
 import Today from "./components/Today/Index";
 
 export default function App() {
-    const [token, setToken] = useState("")
+  const [token, setToken] = useState("");
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login setToken={setToken}/>}></Route>
+        <Route path="/" element={<Login setToken={setToken} />}></Route>
         <Route path="/cadastro" element={<Register />}></Route>
         <Route path="/habitos" element={<Habits />}></Route>
-        <Route path="/hoje" element={<Today token={token}/>}></Route>
+        <Route path="/hoje" element={<Today token={token} />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/Login/Index.js
+++ src/components/Login/Index.js
@@ -1,29 +1,50 @@
 import Container from "./style";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 export default function Login() {
   const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  function handleLogin(e){
+  function handleLogin(e) {
     e.preventDefault();
 
-		const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", {
-			email: email,
-			password: password
-		});
-    promise.then(response => {console.log(response)})
-    promise.catch(error => {alert(error.response.data.message)})
+    const promise = axios.post(
+      "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login",
+      {
+        email: email,
+        password: password,
+      }
+    );
+    promise.then((response) => {
+      console.log(response);
+      navigate("/hoje");
+    });
+    promise.catch((error) => {
+      alert(error.response.data.message);
+    });
   }
 
   return (
     <Container>
       <img src="./assets/LogoImage.png" alt="" />
       <form onSubmit={handleLogin}>
-        <input type="email" placeholder="email" value={email} onChange={e => setEmail(e.target.value)} required/>
-        <input type="password" placeholder="senha" value={password} onChange={e => setPassword(e.target.value)} required/>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
         <button type="submit">Entrar</button>
       </form>
       <Link to="/cadastro">
