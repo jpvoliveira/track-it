c909a27686c2b1e5aff0678aa0f3207fda3e84f1
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2022-01-05 02:50:31 -0300

    Loading funcionando

--- src/components/Login/Index.js
+++ src/components/Login/Index.js
@@ -2,13 +2,18 @@ import Container from "./style";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(true);
+
   const navigate = useNavigate();
 
   function handleLogin(e) {
+    setLoading(false);
     e.preventDefault();
 
     const promise = axios.post(
@@ -18,11 +23,14 @@ export default function Login() {
         password: password,
       }
     );
-    promise.then((response) => {
-      console.log(response);
-      navigate("/hoje");
-    });
+    setTimeout(() => {
+      promise.then((response) => {
+        console.log(response);
+        navigate("/hoje");
+      });
+    }, 1000);
     promise.catch((error) => {
+      setLoading(true);
       alert(error.response.data.message);
     });
   }
@@ -45,11 +53,23 @@ export default function Login() {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-        <button type="submit">Entrar</button>
+        <button type="submit">{loading ? "Entrar" : <Loading />}</button>
       </form>
       <Link to="/cadastro">
         <span>Não tem uma conta? Cadastre-se!</span>
       </Link>
     </Container>
   );
+
+  function Loading() {
+    return (
+      <Loader
+        type="ThreeDots"
+        color="#FFFFFF"
+        height={50}
+        width={50}
+        timeout={3000}
+      />
+    );
+  }
 }
