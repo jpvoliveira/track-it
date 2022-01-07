67907199b9b1428a74cb0ac41a108969ce1c3f7c
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2022-01-07 19:06:27 -0300

    correção erro na importação do useContext

--- src/components/Footer/Index.js
+++ src/components/Footer/Index.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { useContext } from "react/cjs/react.development";
+import { useContext } from "react";
 import HabitsCheckContext from "../../contexts/HabitsCheckContext";
 
 export default function Footer() {
